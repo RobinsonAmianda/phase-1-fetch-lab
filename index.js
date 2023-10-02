d41ac4c8764b02f7d34efd12cf56d3b6a869f1c6
Robinson Amianda <robinsonamianda5@gmail.com>
2023-10-02 07:53:27 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+    if (!response.ok) {
+       Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((books) => {
+    renderBooks(books);
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
 }
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
